aeb17a32f1a48b9a1828180520fb58b1cbab04aa
Andrew KIM <andrewbkim.dev@gmail.com>
2023-10-22 21:55:52 +0900

    Add Clarity Design and test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 import { CoreModule } from 'src/app/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
+    ClarityModule,
     AppRoutingModule,
     PostsModule,
     AdminModule,
