b397886d3277da13b5abfafa727bfb38c6f96662
Andrew KIM <andrewbkim.dev@gmail.com>
2023-10-22 18:13:05 +0900

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'personal-blog-kbi';
+
+  ngOnInit() {
+  }
 }
