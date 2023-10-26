baf7c2444d2599a5e6cb0cfb20b5d1358517ced8
Andrew KIM <andrewbkim.dev@gmail.com>
2023-10-26 20:36:46 +0900

    Add basic markdown text editor/viewer component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,37 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import {
-  ClrCheckboxModule,
-  ClrCommonFormsModule,
-  ClrIconModule,
-  ClrInputModule,
-  ClrTextareaModule
-} from '@clr/angular';
-import { MarkdownModule } from 'ngx-markdown';
 import { CoreModule } from 'src/app/core/core.module';
-
 import { AdminRoutingModule } from './admin-routing.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    MarkdownEditorComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
+    CoreModule,
     AdminRoutingModule,
-    MarkdownModule,
-    ClrCommonFormsModule,
-    ClrTextareaModule,
-    ClrInputModule,
-    ClrCheckboxModule,
-    ClrIconModule,
-    CoreModule
   ]
 })
 export class AdminModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ClarityModule } from '@clr/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  ClarityModule,
+  ClrCheckboxModule,
+  ClrCommonFormsModule,
+  ClrIconModule,
+  ClrInputModule,
+  ClrTextareaModule
+} from '@clr/angular';
+import { MarkdownModule } from 'ngx-markdown';
+
+import { MarkdownEditorComponent } from '../core/markdown-editor/markdown-editor.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ClarityModule,
+    MarkdownModule,
+    ClrCommonFormsModule,
+    ClrTextareaModule,
+    ClrInputModule,
+    ClrCheckboxModule,
+    ClrIconModule,
   ],
   exports: [
     ClarityModule,
+    MarkdownEditorComponent,
     ErrorMessageComponent,
   ],
   declarations: [
+    MarkdownEditorComponent,
     ErrorMessageComponent
   ],
 })
