a92567596a2d2711dc3847ffa33a17e7140e3c92
Byung-In KIM <kimbi@cliensoft.com>
2023-10-27 10:17:14 +0900

    Prep to add angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ClarityModule } from '@clr/angular';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { CoreModule } from 'src/app/core/core.module';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { PostsModule } from './posts/posts.module';
@@ -14,19 +15,26 @@ import { AdminModule } from './admin/admin.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { PostService } from './posts/widgets/post/post.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
   ],
   imports: [
+    // Angular Core Modules
     BrowserModule,
     BrowserAnimationsModule,
+    // For Dev
+    HttpClientInMemoryWebApiModule.forRoot(PostService),
+    // Meta Modules
     CoreModule,
-    ClarityModule,
+    // Feature Modules
     PostsModule,
     AdminModule,
     LayoutModule,
+    // Routing Modules
     AppRoutingModule,
   ],
   providers: [],
