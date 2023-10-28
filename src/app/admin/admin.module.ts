e3afdf4c6c58fee185b47f219d5ae2611e90a6fa
Andrew KIM <andrewbkim.dev@gmail.com>
2023-10-28 14:47:57 +0900

    Refactor file locations

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClarityDesignSystemModule, NgxMarkdownModule } from '../lib';
 import { CoreModule } from 'src/app/core/core.module';
-import { AdminRoutingModule } from './admin-routing.module';
 
+
+import { MarkdownEditorComponent } from './post-editor/markdown-editor/markdown-editor.component';
+import { TagsComponent } from './post-editor/tags/tags.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    MarkdownEditorComponent,
+    TagsComponent,
   ],
   imports: [
     CommonModule,
     CoreModule,
+    NgxMarkdownModule,
+    ClarityDesignSystemModule,
+
     AdminRoutingModule,
   ]
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,19 +5,15 @@ import { RouterLink } from '@angular/router';
 
 import { ClarityDesignSystemModule, NgxMarkdownModule } from '../lib';
 
-import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TopAlertComponent } from './top-alert/top-alert.component';
-import { TagsComponent } from './markdown-editor/tags/tags.component';
 
 @NgModule({
   declarations: [
     ErrorMessageComponent,
-    MarkdownEditorComponent,
     PageNotFoundComponent,
     TopAlertComponent,
-    TagsComponent,
   ],
   imports: [
     CommonModule,
@@ -27,8 +23,8 @@ import { TagsComponent } from './markdown-editor/tags/tags.component';
     RouterLink,
   ],
   exports: [
+    ReactiveFormsModule,
     ErrorMessageComponent,
-    MarkdownEditorComponent,
     PageNotFoundComponent,
     TopAlertComponent,
   ],
