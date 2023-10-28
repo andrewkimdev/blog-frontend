51aafaa3da215c6a908c1efc4483f5cf72a6a507
Andrew KIM <andrewbkim.dev@gmail.com>
2023-10-28 11:58:11 +0900

    Add Markdown cheatsheet link and turn off sanitizer for markdown-generated html string

--- src/app/lib/ngx-markdown.module.ts
+++ src/app/lib/ngx-markdown.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule, SecurityContext } from '@angular/core';
 
 import { ClipboardButtonComponent, ClipboardOptions, MarkdownModule } from 'ngx-markdown';
-
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
       clipboardOptions: {
         provide: ClipboardOptions,
         useValue: {
