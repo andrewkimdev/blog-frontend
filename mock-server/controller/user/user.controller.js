01930d623b8fab4d3208adf58b739f345be59c37
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-02 23:31:18 +0900

    [Server] Add 'getUserById' api endpoint

--- mock-server/controller/user/user.controller.js
+++ mock-server/controller/user/user.controller.js
@@ -6,4 +6,14 @@ router.get('/users', (req, res) => {
   res.json(MockUsers);
 });
 
+router.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = MockUsers.find(user => user.id === id);
+  if (user) {
+      res.json(user);
+  } else {
+    res.status(404);
+  }
+})
+
 module.exports = router;
