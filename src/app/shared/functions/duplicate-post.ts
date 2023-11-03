3edb467200041731ff42d59ce1cda1e14be4dbd5
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-03 19:31:40 +0900

    Set 'updatedAt' each time a property is updated at client

--- src/app/shared/functions/duplicate-post.ts
+++ src/app/shared/functions/duplicate-post.ts
@@ -1,5 +1,12 @@
+import { getCurrentUnixTimeInSeconds } from 'src/app/shared/functions/time.helper';
 import { Post } from '../types';
 
 export const duplicatePost = (post: Post, args?: {}): Post => {
-  return { ...post, tags: [...post.tags], imageIdList: [...post.imageIdList], ...args};
+  return {
+    ...post,
+    tags: [...post.tags],
+    imageIdList: [...post.imageIdList],
+    updatedAt: getCurrentUnixTimeInSeconds() ,
+    ...args,
+  };
 }
