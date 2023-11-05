2b2f682962320d2873cccf716d9c21b739000132
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-05 23:05:02 +0900

    [debugging] working on checkbox logic

--- src/app/post/post-editor/widgets/thumbnail-modal/thumbnail-modal.component.ts
+++ src/app/post/post-editor/widgets/thumbnail-modal/thumbnail-modal.component.ts
@@ -9,9 +9,11 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
-import { Subject, takeUntil, tap } from 'rxjs';
+import { map, Observable, Subject, take, takeUntil, tap } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { selectPost } from 'src/app/post/post-editor/store/post-editor.selector';
+import { Post } from 'src/app/shared/types';
 
 import { ImageFileInfo } from '../image-uploader/image-file-info.interface';
 
@@ -41,6 +43,8 @@ export class ThumbnailModalComponent implements OnInit, OnChanges, OnDestroy {
 
   imageLink = '';
 
+  post$: Observable<Post> = this.store.select(selectPost);
+
   copyButtonText = 'Insert Link'
 
   private destroy$ = new Subject<void>();
@@ -52,14 +56,35 @@ export class ThumbnailModalComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.setInitialCheckboxValue();
     this.isMainImageInputControl.valueChanges.pipe(
       takeUntil(this.destroy$),
-      tap((res) =>
-        res
-          ? this.store.dispatch(PageEditorActions.setMainImage({ imageId: this.fileInfo.id }))
-          : this.store.dispatch(PageEditorActions.unsetMainImage())
-      ),
+      tap((res) =>{
+        if (res){
+          this.store.dispatch(PageEditorActions.setMainImage({ imageId: this.fileInfo.id }))
+        } else if (!res && this.isMainImageInputControl.dirty) {
+          this.store.dispatch(PageEditorActions.unsetMainImage());
+        }
+      }),
     ).subscribe();
+  }
+
+  private setInitialCheckboxValue(): void {
+    // todo - debug the initial checkbox logic
+    this.post$.pipe(
+      take(1),
+      tap((post) => {
+        console.log(this.fileInfo.id);
+        console.log(post.mainImage);
+        console.log(this.fileInfo.id === post.mainImage);
+      }),
+      map((post) => post.mainImage === this.fileInfo.id),
+    ).subscribe((isMainImage) => {
+
+      this.isMainImageInputControl.setValue(isMainImage);
+      this.isMainImageInputControl.markAsPristine();
+      this.isMainImageInputControl.markAsUntouched();
+    });
   }
 
   onCopyClicked() {
@@ -82,6 +107,9 @@ export class ThumbnailModalComponent implements OnInit, OnChanges, OnDestroy {
     const info = changes['fileInfo'];
     if (changes['fileInfo']) {
       this.imageLink = `![image](${ environment.baseUrl }/images/${ info.currentValue.id })`;
+    }
+    if (changes['open'] && changes['open'].currentValue) {
+      this.setInitialCheckboxValue();
     }
   }
 
