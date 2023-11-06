c88cdee9d5ba87f1392890011cf7caab6e249098
Byung-In KIM <kimbi@cliensoft.com>
2023-11-06 16:39:50 +0900

    [Data] Add mock user auth data

---
+++ mock-server/mock-data/auth.js
@@ -0,0 +1,39 @@
+const UserAuth = [
+    {
+        'id': 1,
+        'username': 'alice',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 2,
+        'username': 'bob',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 3,
+        'username': 'charlie',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 4,
+        'username': 'david',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 5,
+        'username': 'evangelist',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 6,
+        'username': 'frank',
+        'password': 'pass0!@#',
+    },
+    {
+        'id': 7,
+        'username': 'grace',
+        'password': 'pass0!@#',
+    },
+];
+
+module.exports = UserAuth;
