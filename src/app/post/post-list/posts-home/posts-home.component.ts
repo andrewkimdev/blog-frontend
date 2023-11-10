211931c84a26f7288888f40101ef963003be3d49
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-10 23:26:07 +0900

    [Debug] Fix url link error that did not show main image on Post List view

--- src/app/post/post-list/posts-home/posts-home.component.ts
+++ src/app/post/post-list/posts-home/posts-home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { map, Observable } from 'rxjs';
 
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
+import { createImageUrlFromUuid, duplicatePost } from 'src/app/shared/functions';
 
 import { Post } from 'src/app/shared/types';
 
@@ -16,11 +18,22 @@ import * as PostsAction from './../store/posts.action';
   styleUrls: ['./posts-home.component.scss']
 })
 export class PostsHomeComponent implements OnInit {
-  posts$: Observable<Post[]> = this.store.pipe(select(selectPosts));
+  posts$: Observable<Post[]> = this.store.pipe(select(selectPosts)).pipe(
+    map((posts: Post[]) => {
+      return posts.map((post: Post) => {
+        const _mainImage = post.mainImage?.startsWith('assets')
+          ? post.mainImage
+          : createImageUrlFromUuid(post.mainImage ?? '', 'html');
+        const mainImage = this.sanitizer.bypassSecurityTrustResourceUrl(_mainImage);
+        return duplicatePost(post, { mainImage });
+      });
+    }),
+  );
 
   constructor(
     private router: Router,
     private store: Store<PostsState>,
+    private sanitizer:  DomSanitizer,
   ) {
   }
 
--- src/app/shared/functions/create-image-url-from-uuid.ts
+++ src/app/shared/functions/create-image-url-from-uuid.ts
@@ -1,5 +1,10 @@
 import { environment } from 'src/environments/environment';
 
-export const createImageUrlFromUuid = (id: string): string => {
-  return `![image](${environment.baseUrl}/images/${id})`;
+export const createImageUrlFromUuid = (id: string, mode: 'markdown' | 'html' = 'markdown'): string => {
+  const url = `${environment.baseUrl}/images/${id}`;
+  if (mode === 'markdown') {
+    return `![image](${environment.baseUrl}/images/${id})`;
+  } else {
+    return url;
+  }
 }
