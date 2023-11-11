d1c652d34ef6fe7f7a69924524d5f78bf78c3a47
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-11 14:33:14 +0900

    [Refactoring] Remove unused module imports in CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
-import { MatChipsModule } from '@angular/material/chips';
-import { MatInputModule } from '@angular/material/input';
-
 import { NgxMarkdownModule } from 'src/app/shared/lib';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -19,8 +16,6 @@ import { TopAlertComponent } from './top-alert/top-alert.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MatChipsModule,
-    MatInputModule,
     NgxMarkdownModule,
     RouterLink,
   ],
