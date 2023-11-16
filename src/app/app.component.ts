24ed911943303ec241d384f6444e8f4719ae45ae
Byung-In KIM <kimbi@cliensoft.com>
2023-11-16 17:48:09 +0900

    Test show for table reading from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { filter } from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
-
+import { supabase } from './shared/lib';
 import * as AuthActions from './account/store/auth.actions'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -20,6 +21,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.redirectToHomeIfAtRoot();
     this.store.dispatch(AuthActions.init());
+    this.randomThing();
+  }
+
+  private randomThing() {
+    console.log('random');
+    const fetchOrders = async () => {
+      try {
+        let { data: orders, error } = await supabase
+          .from('orders')
+          .select('*');
+
+        if (orders) {
+          console.log(orders);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchOrders();
   }
 
   // AppRoutingModule's redirection has a bug so this is my walk-around solution.
